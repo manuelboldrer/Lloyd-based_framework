21560856df68f2fafd574a71e7db3aad1249ed0e
Manuel Boldrer <95948066+manuelboldrer@users.noreply.github.com>
2023-03-16 10:01:45 +0100

    comments

--- MAIN_expl2021_2.m
+++ MAIN_expl2021_2.m
@@ -1,9 +1,31 @@
+%/*-----------------------------------------------------
+%|      Manuel Boldrer, PhD                            |
+%|      Department of Cognitive Robotics               |
+%|      Delft University of Technology                 |
+%|                                                     |
+%|      email: m.boldrer@tudelft.nl                    |
+%|      https://manuelboldrer.github.io/               |                                                   
+%-----------------------------------------------------*/
+%The algorithms implemented in this code were used to generate
+%part of the simulation results in the following papers:
 
-%
-%
-%
-%
-%
+%[1] Boldrer, M., Palopoli, L., & Fontanelli, D. (2022).
+% A unified Lloyd-based framework for multi-agent collective behaviours. 
+% Robotics and Autonomous Systems, 156, 104207.
+
+%[2] Boldrer, M., Palopoli, L., & Fontanelli, D. (2020, October). 
+% Lloyd-based Approach for Robots Navigation in Human-shared environments. 
+% In 2020 IEEE/RSJ International Conference on Intelligent Robots and
+% Systems.
+
+%[3] Boldrer, M., Bevilacqua, P., Palopoli, L., & Fontanelli, D. (2021).
+% Graph connectivity control of a mobile robot network with mixed dynamic multi-tasks. 
+% IEEE Robotics and Automation Letters, 6(2), 1934-1941.
+
+%[4] Boldrer, M., Pasqualetti, F., Palopoli, L., & Fontanelli, D. (2022). 
+% Multiagent persistent monitoring via time-inverted kuramoto dynamics. 
+% IEEE Control Systems Letters, 6, 2798-2803.
+
 
 close all force
 clearvars
